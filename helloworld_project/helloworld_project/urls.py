694365f94ef6e61fd0c8ccca91a2942ef69e6843
elisascodingadventures <144488263+elisascodingadventures@users.noreply.github.com>
2024-02-27 10:25:52 -0500

    Delete helloworld_project directory

--- helloworld_project/helloworld_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Correctly imported 'include' here
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hello.urls')),  # Assuming 'hello' is the name of your app
-]
