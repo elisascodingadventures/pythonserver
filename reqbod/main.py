b3b4cf62127f15dc7f94e16e1025d157901bd00a
elisascodingadventures <144488263+elisascodingadventures@users.noreply.github.com>
2024-03-01 16:28:50 -0500

    this fastapi server can handle data via post
    this is a sample request:
     curl -X 'POST' \
      'http://127.0.0.1:8000/items/' \
      -H 'accept: application/json' \
      -H 'Content-Type: application/json' \
      -d '{
      "name": "Sample Item",
      "description": "This is a sample item",
      "price": 10.5,
      "tax": 1.5
    }'
    {"name":"Sample Item","description":"This is a sample item","price":10.5,"tax":1.5,"price_with_tax":12.0}%                                                                                            
    ll-studio@Purple01 reqbod %

---
+++ reqbod/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
